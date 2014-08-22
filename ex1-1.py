49d29edc6979fe157182f9558d71417888116387
Scott Sperling <ssperlin@yahoo-inc.com>
2014-08-22 00:14:58 -0700

    add comments

--- ex1-1.py
+++ ex1-1.py
@@ -3,7 +3,7 @@ import binascii
 
 given_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-binary = binascii.unhexlify(given_string)
-base64 = binascii.b2a_base64(binary)
+binary = binascii.unhexlify(given_string) # Return the binary data represented by the hexadecimal string.
+base64 = binascii.b2a_base64(binary) # Convert binary data to base64
 
 print base64
